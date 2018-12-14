3e048d8420d1ad2eeecd7158cfbffd58594f3c16
Lei Yang <septembernine@163.com>
2018-12-14 17:30:22 +0800

    add the test main.cc

---
+++ test/main.cc
@@ -0,0 +1,28 @@
+#include "gtest/gtest.h"
+
+#include <string.h>
+
+#include "../src/shcom.hh"
+
+class MkIntfTest: public ::testing::Test {
+ protected:
+  void SetUp() override {
+    retry_time_ = 10.0;
+    retry_interval_ = 1.0;
+    strcpy(emc_nmlfile, "mk.nml");
+  }
+
+ double retry_time_;
+ double retry_interval_;
+
+};
+
+TEST_F(MkIntfTest, TryNml) {
+  int ret = tryNml(retry_time_, retry_interval_);
+  EXPECT_EQ(ret, 0);
+}
+
+int main(int argc, char **argv) {
+  ::testing::InitGoogleTest(&argc, argv);
+  return 0;
+}
